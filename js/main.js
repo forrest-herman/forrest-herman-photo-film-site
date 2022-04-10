5a168e64cfad2e3d79d18f34a14e37335185a704
forrest-herman <forrestherman13@gmail.com>
2022-04-10 17:53:34 -0400

    update gallery animation

--- js/main.js
+++ js/main.js
@@ -102,6 +102,43 @@
         )
     }
 
+    var galleryAnimateIn = function () {
+        var i = 0
+        $(".animate-gallery").waypoint(
+            function (direction) {
+                if (direction === "down" && !$(this.element).hasClass("animated-fast")) {
+                    i++
+
+                    $(this.element).addClass("item-animate")
+                    setTimeout(function () {
+                        $("body .animate-gallery.item-animate").each(function (k) {
+                            var el = $(this)
+                            setTimeout(
+                                function () {
+                                    var effect = el.data("animate-effect")
+                                    if (effect === "fadeIn") {
+                                        el.addClass("fadeIn animated-fast")
+                                    } else if (effect === "fadeInLeft") {
+                                        el.addClass("fadeInLeft animated-fast")
+                                    } else if (effect === "fadeInRight") {
+                                        el.addClass("fadeInRight animated-fast")
+                                    } else {
+                                        el.addClass("fadeInUp animated-fast")
+                                    }
+
+                                    el.removeClass("item-animate")
+                                },
+                                k * 125,
+                                "easeInOutExpo"
+                            )
+                        })
+                    }, 100)
+                }
+            },
+            { offset: "90%" }
+        )
+    }
+
     var dropdown = function () {
         $(".has-dropdown")
             .mouseenter(function () {
@@ -202,6 +239,8 @@
         loaderPage()
         counter()
         counterWayPoint()
+
+        galleryAnimateIn()
     })
 })()
 
